c9e608bc74a291efd2aad6c27a5275c4773a913e
Киняпин Сергей <sergeykinn@mail.ru>
2019-06-27 18:40:45 +0300

    +changes

---
+++ pages/locators.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_LINK), " error LOGIN_LINK "
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), " error LOGIN_FORM "
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), " error REGISTER_FORM "
